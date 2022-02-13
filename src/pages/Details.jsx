b279fd2653d1a4434a79fbb129ea5bfccc8919fc
Pavel07-chyden <pates1993@mail.ru>
2022-02-13 17:26:15 +0300

    first commit

---
+++ src/pages/Details.jsx
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import { IoArrowBack } from 'react-icons/io5';
+import { searchByCountry } from '../config';
+import { Button } from '../components/Button';
+import { Info } from '../components/Info';
+import { useParams, useHistory  } from 'react-router-dom';
+
+export const Details = () => {
+  const { name } = useParams();
+  const { push, goBack } = useHistory();
+  const [country, setCountry] = useState(null);
+
+  console.log(country);
+
+  useEffect(() => {
+    axios.get(searchByCountry(name)).then(({ data }) => setCountry(data[0]));
+  }, [name]);
+
+  return (
+    <div>
+      <Button onClick={goBack}>
+        <IoArrowBack /> Back
+      </Button>
+      {country && <Info push={push} {...country} />}
+    </div>
+  );
+};
